5906dad1e9c4a324266c3f8b19958c0a5f43ce2a
shayan <sak704@nyu.edu>
2022-03-02 17:08:19 -0800

    commit tree 99dd2fc043203537c1a6571e0a586d8b37290f8c

---
+++ src/00-objectball.js
@@ -0,0 +1,121 @@
+function gameObject() {
+    let stats={};
+
+    stats.home = {teamName: "Brooklyn Nets", colors: "Black, White", players: {
+        "Alan Anderson": {
+            number: "0",
+            shoe: "16",
+            points: "22",
+            rebounds: "12",
+            assists: "12",
+            steals: "3",
+            blocks: "1",
+            slamDunks: "1" 
+        },
+
+        "Reggie Evans": {
+            number: "30",
+            shoe: "14",
+            points: "12",
+            rebounds: "12",
+            assists: "12",
+            steals: "12",
+            blocks: "12",
+            slamDunks: "7" 
+        },
+
+        "Brook Lopez": {
+            number: "31",
+            shoe: "17",
+            points: "17",
+            rebounds: "19",
+            assists: "10",
+            steals: "3",
+            blocks: "1",
+            slamDunks: "15" 
+        },
+
+        "Mason Plumlee": {
+            number: "1",
+            shoe: "19",
+            points: "26",
+            rebounds: "12",
+            assists: "6",
+            steals: "3",
+            blocks: "8",
+            slamDunks: "5"
+        },
+
+        "Jason Terry": {
+            number: "31",
+            shoe: "15",
+            points: "19",
+            rebounds: "2",
+            assists: "2",
+            steals: "4",
+            blocks: "11",
+            slamDunks: "1",
+        }}};
+
+    stats.away = {teamName: "Charlotte Hornets", colors: "Turquoise, Purple", players: {
+        "Jeff Adrien": {
+            number: "0",
+            shoe: "16",
+            points: "22",
+            rebounds: "12",
+            assists: "12",
+            steals: "3",
+            blocks: "1",
+            slamDunks: "1" 
+        },
+
+        "Bismak Biyombo": {
+            number: "30",
+            shoe: "14",
+            points: "12",
+            rebounds: "12",
+            assists: "12",
+            steals: "12",
+            blocks: "12",
+            slamDunks: "7" 
+        },
+
+        "DeSagna Diop": {
+            number: "31",
+            shoe: "17",
+            points: "17",
+            rebounds: "19",
+            assists: "10",
+            steals: "3",
+            blocks: "1",
+            slamDunks: "15" 
+        },
+
+        "Ben Gordon": {
+            number: "1",
+            shoe: "19",
+            points: "26",
+            rebounds: "12",
+            assists: "6",
+            steals: "3",
+            blocks: "8",
+            slamDunks: "5",
+        },
+
+        "Brendan Haywood": {
+            number: "31",
+            shoe: "15",
+            points: "19",
+            rebounds: "2",
+            assists: "2",
+            steals: "4",
+            blocks: "11",
+            slamDunks: "1",
+        }
+
+        
+    }};
+
+
+    console.log (stats);
+}
